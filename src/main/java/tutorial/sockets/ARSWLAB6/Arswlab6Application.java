d483a7010daffb910d3a5306599a8d681458bb89
juan-cely-l <juan.cely-l@mail.escuelaing.edu.co>
2025-10-28 18:25:24 -0500

    add:initial commit

---
+++ src/main/java/tutorial/sockets/ARSWLAB6/Arswlab6Application.java
@@ -0,0 +1,13 @@
+package tutorial.sockets.ARSWLAB6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arswlab6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arswlab6Application.class, args);
+	}
+
+}
---
+++ src/test/java/tutorial/sockets/ARSWLAB6/Arswlab6ApplicationTests.java
@@ -0,0 +1,13 @@
+package tutorial.sockets.ARSWLAB6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arswlab6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
